d1c3faf2956442a2fd4ab638f16febb236115d9c
Hawraa Al Qassim <hawraa28ahmed@gmail.com>
2024-03-29 05:48:09 +0300

    maximum code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.*;;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            Scanner input = new Scanner(System.in);
+    
+            System.out.println("Enter the first Number");
+            int a = input.nextInt();
+    
+            System.out.println("Enter the second number");
+            int b = input.nextInt();
+    
+            if(a>b){
+                System.out.println("The maximum number is:" + a);
+            }
+    
+            else if(a<b){
+                System.out.println("The maximum number is:" + b);
+            }
+    
+            else{
+                System.out.println("the two numbers are equals");
+            }
+    
+        
     }
 }
